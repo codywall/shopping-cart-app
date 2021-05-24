e2d141f5b10c8b58ade34f508e438bb469b0ac1c
codywall <cody.wall@gmail.com>
2021-05-24 00:18:20 -0700

    Add loaded event listener

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function () {
   // Fetch products and build product listings with them
   const resultsWrapper = document.querySelector("#results");
   if (resultsWrapper) {
